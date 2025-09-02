a94bee75320f535d1e0c43a574b806d0dcd46470
Lautaro cores <Lautaro.cores09@gmail.com>
2025-09-02 10:50:50 -0300

    Delete TrabajoFoodmasters directory

--- TrabajoFoodmasters/InicioSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "conexion.php";
-
-$sql = "SELECT * FROM usuarios WHERE pais='Argentina'";
-$res = mysqli_query($con, $sql);
-$arr = [];
-while ($fila = mysqli_fetch_assoc($res)) {
-    $arr[] = $fila["nombre"];
-}
--- TrabajoFoodmasters/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$bd = "FoodMasters";
-
-$con = mysqli_connect($server, $user, $pass, $bd);
