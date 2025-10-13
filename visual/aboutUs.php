95dd46f27d698e55f59166b80834cd719df79362
Mateo Medina <mateomedina08@gmail.com>
2025-10-13 09:21:47 -0300

    Pagina sobe nosotros + imagenes

---
+++ visual/aboutUs.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myfoods - aboutUs</title>
+</head>
+<body>
+<h1><p>¡Sobre nosotros!</p></h1>
+<br>
+<!--Falta conectarlo con otra pagina para venir aca -->
+<div>
+    <img src="..\img\yea.jpg" alt="" width="200" height="200">
+    <p>Mateo, un programador de php</p>
+<br>
+    <img src="..\img\tlabaja.jpg" alt="" width="200" height="200">
+    <p>Cores, un programador de php</p>
+<br>
+    <img src="..\img\agustina.jpg" alt="" width="200" height="200">
+    <p>Agustina, administradora de base de datos</p>
+<br>
+    <img src="..\img\Thomas.webp" alt="" width="200" height="200">
+    <p>Thomas, un encargado de visuales (css) que vive en estados unidos y pichincha</p>
+<br>
+    <img src="..\img\chopper.webp" alt="" width="200" height="200">
+    <p>victoria, una encargada de visuales (css) que vive en Boedo e Yrigoyen </p>
+<br>
+    <img src="..\img\perroShh.png" alt="" width="200" height="200" >
+    <p>Pietro, un programador de html</p>
+<br>
+    <img src="..\img\martin.png" alt="" width="200" height="200">
+    <p>martin</p>
+<br>
+</div>
+</body>
+</html>
