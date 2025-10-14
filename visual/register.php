7f712fca54ea31e6dc552013ee57e6443ef3150d
Lautaro cores <Lautaro.cores09@gmail.com>
2025-10-14 02:12:50 -0300

    agregar css de registrar y iniciar secion

--- visual/register.php
+++ visual/register.php
@@ -8,20 +8,39 @@
      <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- CSS principal -->
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/styleAG.css">
     <!-- Bootstrap JS y Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
 </head>
 </head>
 
 <body>
-    <h2>Registrarse</h2>
-    <form id="formRegister">
-        <input type="text" name="userName" id="regUserName" placeholder="Nombre de usuario" required autocomplete="username">
-        <input type="email" name="userEmail" id="regUserEmail" placeholder="Correo electrónico" required autocomplete="email">
-        <input type="password" name="userPassword" id="regUserPassword" placeholder="Contraseña" required minlength="5" autocomplete="new-password">
-        <button type="submit" id="Registrar">Registrarse</button>
-    </form>
+
+    <div><img src="../img/logo.png" alt="Myfoods Logo" id="logo"></div>
+    
+    <h2>Registrate en Myfoods</h2>
+    <br>
+    
+    <div>
+        <form id="formRegister">
+            
+            <p>Email</p>
+            <input type="email" name="userEmail" id="regUserEmail" class="name" placeholder="Correo electrónico" required autocomplete="email">
+            <br>
+            
+            <p>Contraseña</p>
+            <input type="password" name="userPassword" id="regUserPassword" class="name" placeholder="Contraseña" required minlength="5" autocomplete="new-password">
+            <br>
+            
+            <p>Nombre de usuario</p>
+            <input type="text" name="userName" id="regUserName" class="name" placeholder="Nombre de usuario" required autocomplete="username">
+            <br>
+            <input type="submit" id="Registrar" value="Crear cuenta" class="styled-button-register">
+            </form>
+    </div>
+
+    
+
     <div id="mensaje"></div>
     <br>
     <p>¿Ya tienes una cuenta? <a href="logIn.php">Inicia sesión aquí</a></p>
