a29d42ef13bad0a8ab741a2472b3b445ec89b73b
Mateo Medina <mateomedina08@gmail.com>
2025-11-04 08:37:18 -0300

    subo area de trivia para la exposuiza

--- visual/exposuiza.php
+++ visual/exposuiza.php
@@ -13,6 +13,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
   <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
 
+  <a  href="../visual/trivia.php"><img id="fototrivia" src="../img/istockphoto-1303554344-612x612.jpg" alt="Logo"></a>
+
 <body>
     <?php include '../includes/navbar.php'; ?>
     <?php include '../includes/backButton.php'; ?>
